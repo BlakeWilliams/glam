6a73af2d30677de64bae6595629a508b52b3e824
Blake Williams <blake@blakewilliams.me>
2024-09-02 21:05:29 -0400

    Remove unused tests

--- template/template_test.go
+++ template/template_test.go
@@ -24,77 +24,14 @@ var wrapperTemplate = `<div>
 </div>
 `
 
-// func (wc *WrapperComponent) Render(w io.Writer) {
-// 	w.Write([]byte(fmt.Sprintf("<div>Name: %s\nAge: %d\n%s</div>", wc.Name, wc.Age, wc.Children)))
-// }
-
 type NestedComponent struct {
 	Children template.HTML
 }
-
-// func (nc *NestedComponent) Render(w io.Writer) {
-// 	w.Write([]byte(fmt.Sprintf("<article>%s</article>", nc.Children)))
-// }
 
 var nestedTemplate = `<article>
 	{{.Children}}
 </article>
 `
-
-// func TestTemplateParse(t *testing.T) {
-// 	parser := &TemplateParser{
-// 		components: map[string]bool{
-// 			"Greeter": true,
-// 		},
-// 	}
-
-// 	// err := parser.ParseTemplate(strings.NewReader(`<b>Hello <Yell name="{.Name}"></Yell></b>`))
-// 	err := parser.ParseTemplate("main", `
-// 	{{ range $index, $element := .Value }}
-// 	 	<b>Hi {{.Foo}}</b>
-// 	{{ end }}
-// 	`)
-// 	require.NoError(t, err)
-
-// 	require.Fail(t, "omg")
-// }
-
-// func TestTemplateParse_WithContent(t *testing.T) {
-// 	parser := &Engine{
-// 		components: map[string]reflect.Value{
-// 			"WrapperComponent": reflect.ValueOf(WrapperComponent{}),
-// 		},
-// 	}
-
-// 	_, err := parser.ParseTemplate("main.go", `<b>Hello <WrapperComponent>Foo</WrapperComponent></b>`)
-// 	require.NoError(t, err)
-
-// 	require.Fail(t, "omg")
-// }
-
-// func TestTemplateParse_omg(t *testing.T) {
-// 	template := &Template{Name: "test"}
-// 	template.Parse(`<b>Hello <WrapperComponent>Foo</WrapperComponent></b>`, map[string]reflect.Value{
-// 		"WrapperComponent": reflect.ValueOf(WrapperComponent{}),
-// 	})
-// 	// require.NoError(t, err)
-
-// 	fmt.Println(template.String())
-
-// 	require.Fail(t, "omg")
-// }
-
-// func TestTemplateParse_Attributes(t *testing.T) {
-// 	template := &Template{Name: "test"}
-// 	template.Parse(`<b>Hello <WrapperComponent rad name="Fox Mulder">Foo</WrapperComponent></b>`, map[string]reflect.Value{
-// 		"WrapperComponent": reflect.ValueOf(WrapperComponent{}),
-// 	})
-// 	// require.NoError(t, err)
-
-// 	fmt.Println(template.String())
-
-// 	require.Fail(t, "omg")
-// }
 
 func TestTemplateParse_Nested(t *testing.T) {
 	engine := New(nil)
